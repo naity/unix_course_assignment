75f6762b6f22fc4171701457302b58e2ed347bda
naity <yuan.allen.tian@gmail.com>
2017-11-15 17:38:30 -0800

    updated files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Peer-graded Assignment: Bash, Make, Git, and GitHub
 ## The date and time at which make was run: 
-Wed Nov 15 17:24:58 PST 2017
+Wed Nov 15 17:38:13 PST 2017
 ## The number of lines of code contained in guessinggame.sh:
-23
+21
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,21 @@
-num_files=$(ls | wc -l)
-
-function guess_files {
-    echo "Please guess how many files are in the current directory:"
-
-    while :
-    do
-        read guess
-
-        if [[ $guess -eq $num_files ]]
-        then
-            break
-        elif [[ $guess -lt $num_files ]]
-        then
-            echo "Your guess is too low"
-        else
-            echo "Your guess is too high"
-        fi
-    done
-
-    echo "Congradulations! You've guessed it right!"
+function compare {
+   if [[ $guess -lt $num_files ]]
+    then
+        echo "Your guess is too low"
+    else
+        echo "Your guess is too high"
+    fi
 }
 
-guess_files
+num_files=$(ls | wc -l)
+
+echo "Please guess how many files are in the current directory:"
+read guess
+
+while [[ $guess -ne $num_files ]]
+do
+    compare
+    read guess
+done
+
+echo "Congradulations! You've guessed it right!"
